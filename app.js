696f64b8de0af9e873402af333736027273a378e
Q <qdevelop@hotmail.com>
2018-10-03 00:46:13 +0900

    p71 htmlの出力

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http')
 var server = http.createServer(
     
     function(request,response){
-        response.end("Hello, world!")
+        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>')
     }
 );
 
