a377d7c3e9758a927bfc96c58692616a19d7b7db
Q <qdevelop@hotmail.com>
2018-10-03 00:56:14 +0900

    P75 htmlで日本語を表示

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const http = require('http')
 var server = http.createServer(
     
     function(request,response){
-        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>')
+        response.setHeader('Content-Type','text/html')
+        response.write('<!DOCTYPE html><html lang="ja">')
+        response.write('<head><meta charset="utf-8">')
+        response.write('<title>Hello</title></head>')
+        response.write('<body><h1>Hello</h1>')
+        response.write('<p>これはnode.jsのサンプルページです</p>','utf8')
+        response.write('</body></html>')
     }
 );
 
-server.listen(3000)
+server.listen(3000);
+console.log("Server started.")
